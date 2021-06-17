bd88ac277343b875fb6cd4884ca82c74291267ff
Pranoy <pranoy.mbstu12001@gmail.com>
2021-06-17 18:58:57 +0600

    updated query

--- src/main/java/com/pranoy/bookstore/repository/UserRepository.java
+++ src/main/java/com/pranoy/bookstore/repository/UserRepository.java
@@ -1,2 +1,12 @@
-package com.pranoy.bookstore.repository;public interface UserRepository {
+package com.pranoy.bookstore.repository;
+
+import com.pranoy.bookstore.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUserName(String username);
+    User findByEmail(String email);
+    List<User> findAll();
 }
